d04736c5c8d34d149400fcff10b92f307f0300da
Hiro Protagonist <shdesai118@gmail.com>
2021-10-22 14:12:01 -0600

    added comments on what to do

---
+++ index.js
@@ -0,0 +1,7 @@
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+// create an command line application that dunamically generates a professional ReadMe
+
+// requirements
+//project title , description, table of content, installation, usage info, tests and questions
