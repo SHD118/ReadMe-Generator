3916c01b1844a4fc65ea1bc7cff09194a3dafc52
Hiro Protagonist <shdesai118@gmail.com>
2021-10-22 14:23:47 -0600

    added inqurier code

--- index.js
+++ index.js
@@ -5,3 +5,48 @@ const fs = require('fs');
 
 // requirements
 //project title , description, table of content, installation, usage info, tests and questions
+
+// implement user input collection via inquirer
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What do you want to call the repository?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Please add a description'
+        },
+
+        {
+            type: 'input',
+            name: 'tOC',
+            message: 'Please add a table of content'
+        },
+        {
+            type: 'input',
+            name: 'installation',
+            message: 'Please at installation instructions'
+        },
+        {
+            type: 'input',
+            name: 'usage',
+            message: 'Please add detail on the usage detail'
+        },
+        {
+            type: 'input',
+            // table of content
+            name: 'test',
+            message: 'Please add any testing information'
+        },
+        {
+            type: 'input',
+            // table of content
+            name: 'questions',
+            message: 'Please add any questions'
+        },
+    ])
+}
